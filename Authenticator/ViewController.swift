738c939003dffeb38354cea2a79b3be7614c7edb
Narek Ghukasyan <narek.ghukasyan333@gmail.com>
2023-11-13 15:09:28 +0400

    Initial Commit

---
+++ Authenticator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authenticator
+//
+//  Created by PRO 14 on 13.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
